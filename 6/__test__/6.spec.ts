680946e9dca8a3704ab21c355e76805fa5911753
Jake Carrington <jake.carrington@ditto.ai>
2019-12-09 19:34:42 +0000

    Day 6 - orbits - part 2

--- 6/__test__/6.spec.ts
+++ 6/__test__/6.spec.ts
@@ -1,52 +1,58 @@
-import { totalOrbits, orbitalTransfers } from "../src";
-import input from "../input.json";
+import { totalOrbits, orbitalTransfers } from '../src';
+import input from '../input.json';
 
-describe("Day 0", () => {
-  describe("totalOrbits", () => {
-    it("should equal 42", () => {
+describe('Day 0', () => {
+  describe('totalOrbits', () => {
+    it('should equal 42', () => {
       const result = totalOrbits([
-        "COM)B",
-        "B)C",
-        "C)D",
-        "D)E",
-        "E)F",
-        "B)G",
-        "G)H",
-        "D)I",
-        "E)J",
-        "J)K",
-        "K)L"
+        'COM)B',
+        'B)C',
+        'C)D',
+        'D)E',
+        'E)F',
+        'B)G',
+        'G)H',
+        'D)I',
+        'E)J',
+        'J)K',
+        'K)L'
       ]);
       expect(result).toEqual(42);
     });
 
-    it("puzzle input should equal 144909", () => {
+    it('puzzle input should equal 144909', () => {
       const result = totalOrbits(input);
       expect(result).toEqual(144909);
     });
   });
 
-  describe("orbitalTransfers", () => {
-    it("does something", () => {
+  describe('orbitalTransfers', () => {
+    it('example should be 4 jumps', () => {
       const result = orbitalTransfers(
         [
-          "COM)B",
-          "B)C",
-          "C)D",
-          "D)E",
-          "E)F",
-          "B)G",
-          "G)H",
-          "D)I",
-          "E)J",
-          "J)K",
-          "K)L",
-          "K)YOU",
-          "I)SAN"
+          'COM)B',
+          'B)C',
+          'C)D',
+          'D)E',
+          'E)F',
+          'B)G',
+          'G)H',
+          'D)I',
+          'E)J',
+          'J)K',
+          'K)L',
+          'K)YOU',
+          'I)SAN'
         ],
-        "YOU",
-        "SAN"
+        'YOU',
+        'SAN'
       );
+      expect(result).toBe(4);
+    });
+
+    it('puzzle input', () => {
+      const result = orbitalTransfers(input, 'YOU', 'SAN');
+      expect(result).toBe(259);
     });
   });
 });
--- 6/src/index.ts
+++ 6/src/index.ts
@@ -1,4 +1,4 @@
-import util from "util";
+import util from 'util';
 
 interface Objects {
   [key: string]: string[];
@@ -7,7 +7,7 @@ interface Objects {
 const createObjectMap = (input: string[]) => {
   const objects: Objects = {};
   input.forEach(object => {
-    const [obj, orbits] = object.split(")");
+    const [obj, orbits] = object.split(')');
     if (!objects[obj]) objects[obj] = [orbits];
     else if (objects[obj]) objects[obj].push(orbits);
   });
@@ -16,7 +16,7 @@ const createObjectMap = (input: string[]) => {
 
 export const totalOrbits = (input: string[]) => {
   const objects = createObjectMap(input);
-  const tree = mapToTree("COM", objects, objects["COM"], null);
+  const tree = mapToTree('COM', objects, objects['COM'], null);
   return getTotalOrbits(tree, 0);
 };
 
@@ -26,15 +26,19 @@ export const orbitalTransfers = (
   goal: string
 ) => {
   const objects: Objects = createObjectMap(input);
-  const tree = mapToTree("COM", objects, objects["COM"], null);
-  const you = dfs()(tree, "YOU");
-  const san = dfs()(tree, "SAN");
+  const tree = mapToTree('COM', objects, objects['COM'], null);
+  const you = dfs()(tree, start);
+  console.log(you);
+  const san = dfs()(tree, goal);
+  if (you && san) {
+    return findNumberOfJumps(you, san);
+  } else return false;
 };
 
-const findCommonAncestor = (start: Tree, goal: Tree) => {
-  const startAncestors: Tree[] = [];
-  const goalAncestors: Tree[] = [];
-  let ancestorFound = false;
+const findNumberOfJumps = (start: Tree, goal: Tree) => {
+  const startAncestors: string[] = [];
+  const goalAncestors: string[] = [];
+  let match: string | false = false;
   const startStack: Tree[] = [];
   const goalStack: Tree[] = [];
   startStack.push(start);
@@ -43,7 +47,37 @@ const findCommonAncestor = (start: Tree, goal: Tree) => {
   while (startStack.length > 0 || goalStack.length > 0) {
     let startCurrent = startStack.pop();
     let goalCurrent = goalStack.pop();
+    if (startCurrent && startCurrent.parent)
+      startAncestors.push(startCurrent.parent.name);
+    if (goalCurrent && goalCurrent.parent)
+      goalAncestors.push(goalCurrent.parent.name);
+    const matched = startAncestors.filter(ancestor =>
+      goalAncestors.includes(ancestor)
+    );
+    if (matched.length > 0) {
+      match = matched[0];
+    } else {
+      if (startCurrent && startCurrent.parent)
+        startStack.push(startCurrent.parent);
+      if (goalCurrent && goalCurrent.parent) goalStack.push(goalCurrent.parent);
+    }
   }
+  if (match) {
+    const startCount = startAncestors
+      .join(',')
+      .split(',' + match)[0]
+      .split(',').length;
+    const goalCount = goalAncestors
+      .join(',')
+      .split(',' + match)[0]
+      .split(',').length;
+    return startCount + goalCount;
+  }
+
+  console.log(startAncestors.toString());
+  console.log(startAncestors);
+
+  return match;
 };
 
 const dfs = () => {
