365ac0cab6c361c91d8eea436142c1f2354e6106
Jake Carrington <jake.carrington@ao.com>
2019-12-09 14:14:53 +0000

    Day 8 - image - part 2

--- 8/__test__/8.spec.ts
+++ 8/__test__/8.spec.ts
@@ -1,9 +1,49 @@
-import { fn } from "../src";
+import { fn, asColumns, finalImage } from "../src";
 import input from "../input.json";
 
 describe("Day 8", () => {
   it("Should get number of ones * number of twos in layer with least zeroes", () => {
     const result = fn(input);
     expect(result).toBe(2080);
+  });
+
+  it("finalImage", () => {
+    const result = finalImage(input);
+  });
+
+  it("should convert to columns", () => {
+    const input = [
+      [
+        [1, 2, 3, 0],
+        [4, 5, 6, 0],
+        [7, 8, 9, 0]
+      ],
+      [
+        [11, 12, 13, 10],
+        [14, 15, 16, 10],
+        [17, 18, 19, 10]
+      ]
+    ];
+    const result = asColumns(input);
+    expect(result).toEqual([
+      [
+        [1, 11],
+        [2, 12],
+        [3, 13],
+        [0, 10]
+      ],
+      [
+        [4, 14],
+        [5, 15],
+        [6, 16],
+        [0, 10]
+      ],
+      [
+        [7, 17],
+        [8, 18],
+        [9, 19],
+        [0, 10]
+      ]
+    ]);
   });
 });
--- 8/src/index.ts
+++ 8/src/index.ts
@@ -7,6 +7,40 @@ export const fn = (input: string) => {
   return ones * twos;
 };
 
+export const finalImage = (input: string) => {
+  const digits = input.split("").map(digit => parseInt(digit));
+  const levels = chunk(digits, 25);
+  const layers = chunk(levels, 6);
+  const columns = asColumns(layers);
+  let removedTrans = columns.map(level => {
+    return level.map(column => {
+      return column.filter(digit => digit !== 2);
+    });
+  });
+  const topPixels = removedTrans.map(level => {
+    return level.map(column => column[0]);
+  });
+  console.log(topPixels);
+  const merged = topPixels.map(level => level.join("")).join("");
+  console.log(merged);
+};
+
+export const asColumns = (layers: number[][][]) => {
+  let lay = [];
+  for (let level = 0; level < layers[0].length; level++) {
+    let row = [];
+    for (let digit = 0; digit < layers[0][0].length; digit++) {
+      let col = [];
+      for (let layer = 0; layer < layers.length; layer++) {
+        col.push(layers[layer][level][digit]);
+      }
+      row.push(col);
+    }
+    lay.push(row);
+  }
+  return lay;
+};
+
 const chunk = <T>(array: T[], size: number) => {
   const chunks: T[][] = [];
   let chunk: T[] = [];
